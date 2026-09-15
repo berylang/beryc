587162c88e1cb79c7ce2d7d087a3448167665636
vitthalhumbe <vitthal.3454@gmail.com>
2026-09-15 22:10:37 +0530

    Done examine - SemanticAnalyzer::analyzeNode(node)

--- src/sema/sema.cpp
+++ src/sema/sema.cpp
@@ -112,23 +112,37 @@ void SemanticAnalyzer::analyze() {
 }
 
 void SemanticAnalyzer::analyzeNode(ASTNode* node) {
+
+    /*
+    
+    One can identify this function as the main dispatcher.
+    it determines what kinda of AST node it has received and sends it to the corresponding semantic analysis funciton.
+    this keeps the handling of different node types separate instead of putting all 
+    the analysis in one large function. 
+
+    so every node type gets it's own analysis function for maintainable code.
+    
+    */
     if (node->type == NodeType::VAR_DECL)               analyzeVarDecl(node);
     else if (node->type == NodeType::ARRAY_DECL)        analyzeArrayDecl(node);
-    else if (node->type == NodeType::ASSIGNMENT_EXPR || 
-             node->type == NodeType::CALL_EXPR || 
-             node->type == NodeType::UNARY_EXPR)   typeChecker.analyzeExpression(node);
-    else if(node->type == NodeType::IF_STMT)            analyzeIfStmt(node);
+    else if (node->type == NodeType::IF_STMT)           analyzeIfStmt(node);
     else if (node->type == NodeType::WHILE_STMT)        analyzeWhileStmt(node);
     else if (node->type == NodeType::DOWHILE_STMT)      analyzeDoWhileStmt(node);
     else if (node->type == NodeType::FOR_STMT)          analyzeForStmt(node);
     else if (node->type == NodeType::FUNC_DEF)          analyzeFuncDef(node);
     else if (node->type == NodeType::RETURN_STMT)       analyzeReturnStmt(node);
+    else if (node->type == NodeType::BREAK_STMT)        analyzeBreakStmt(node);
     else if (node->type == NodeType::CONTINUE_STMT)     analyzeContinueStmt(node);
     else if (node->type == NodeType::SWITCH_STMT)       analyzeSwitchStmt(node);
     else if (node->type == NodeType::ENUM_DECL)         analyzeEnumDecl(node);
-    else if (node->type == NodeType::FOR_STMT)          analyzeForStmt(node);
     else if (node->type == NodeType::FOR_IN_STMT)       analyzeForInStmt(node);
     else if (node->type == NodeType::CLASS_DEF)         analyzeClassDecl(node);
+
+    // These nodes don't require any semantic analysis at this stage.
     else if (node->type == NodeType::PASS_STMT){}
     else if (node->type == NodeType::EXTERN_DECL){}
-} 
+
+    // Expressions that can affect types are passed to the type checker so their types and other semantic rules can be validated.
+    else if (node->type == NodeType::ASSIGNMENT_EXPR || node->type == NodeType::CALL_EXPR ||  node->type == NodeType::UNARY_EXPR)   
+        typeChecker.analyzeExpression(node);
+}
